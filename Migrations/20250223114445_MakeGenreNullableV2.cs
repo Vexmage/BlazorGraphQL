6d30ad3a7a8748323d49b8cf678a39101ad8813c
Joel Southall <vextechmage@gmail.com>
2026-06-25 00:10:50 -0700

    chore: force-index core application structure, restore repository file tree, and update clean blazor graphql modules

---
+++ Migrations/20250223114445_MakeGenreNullableV2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorGraphQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeGenreNullableV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
